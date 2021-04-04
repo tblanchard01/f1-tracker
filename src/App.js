15875d3f2cad0029e86046be40007c254e0bd71b
Tim <tim.blanchard@hotmail.co.uk>
2021-04-04 10:22:10 +0100

    data fetch working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [schedule, setSchedule] = useState({})
+
+  useEffect(() => {
+    async function getSchedule(){
+      const data = await axios.get('http://ergast.com/api/f1/2021.json')
+      setSchedule(data)
+
+    }
+    getSchedule()
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{JSON.stringify(schedule)} </div>
     </div>
   );
 }
