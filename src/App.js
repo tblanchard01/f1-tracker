0a4fcf788b14a26d84243417c52ebbebf1fdb8ee
Tim <tim.blanchard@hotmail.co.uk>
2021-04-05 09:39:45 +0100

    extract data getter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import "./App.css";
-import axios from "axios";
+import getData from "./getData";
 import React, { useState, useEffect } from "react";
-
-const url = "http://ergast.com/api/f1/2021.json";
 
 export default function App() {
   const [schedule, setSchedule] = useState({});
-  async function getData() {
-    try {
-      const data = await axios.get(url);
-      setSchedule(data);
-    } catch (e) {
-      // console.log(e);
-    }
-  }
+  const getSchedule = () => getData(setSchedule);
   useEffect(() => {
-    getData();
+    getSchedule();
   }, []);
   return <div className="App">{JSON.stringify(schedule)}</div>;
 }
