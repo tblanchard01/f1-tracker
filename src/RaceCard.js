69945251c9515fd7d75d57ed63a1025bc86c9991
Tim <tim.blanchard@hotmail.co.uk>
2021-04-10 17:55:08 +0100

    d8 formatting

--- src/RaceCard.js
+++ src/RaceCard.js
@@ -1,7 +1,8 @@
+import relativeTimeMapper from './helpers/relativeTimeMapper'
 export default function RaceCard ({raceName, date, time}){
     return <div>
         <h1>{raceName}</h1>
-        <h2>{date}</h2>
-        <h3>{time}</h3>
+        <h2>{relativeTimeMapper({date, time})}</h2>
+       
     </div>
 }
--- src/helpers/relativeTimeMapper.js
+++ src/helpers/relativeTimeMapper.js
@@ -1,5 +1,6 @@
 export default function relativeTimeMapper({date, time}){
-    const d = new Date(`${date} ${time} UTC`).toString().split(' ').slice(1,5)
+    let d = new Date(`${date} ${time} UTC`).toString().split(' ').slice(1,5)
     d[d.length-1] = d[d.length-1].split("").slice(0,5).join("")
-    return d.join(' '); 
+    d.splice(3, 0, " at")
+    return d.join(" "); 
 }
