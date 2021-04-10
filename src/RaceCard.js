7d9e0b63cf951fa7e837ca028b8e576c21e227ac
Tim Blanchard <timothy.blanchard@tesco.com>
2021-04-10 21:17:56 +0100

    wip

--- src/RaceCard.js
+++ src/RaceCard.js
@@ -1,8 +1,13 @@
-import relativeTimeMapper from './helpers/relativeTimeMapper'
-export default function RaceCard ({raceName, date, time}){
-    return <div>
-        <h1>{raceName}</h1>
-        <h2>{relativeTimeMapper({date, time})}</h2>
-       
+import relativeTimeMapper from "./helpers/relativeTimeMapper";
+import deltaTimeMapper from "./helpers/deltaTimeMapper";
+
+export default function RaceCard({ raceName, date, time }) {
+    // TODO get todays date in UTC 
+  return (
+    <div>
+      <h1>{raceName}</h1>
+      <h2>{relativeTimeMapper({ date, time })}</h2>
+      <h3>{deltaTimeMapper({ date })}</h3> 
     </div>
+  );
 }
--- src/helpers/deltaTimeMapper.js
+++ src/helpers/deltaTimeMapper.js
@@ -1,5 +1,5 @@
 export default function getTimeDelta({date: firstDate},  secondDate = new Date().toString()) {
   const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-return Math.round(Math.abs((new Date(firstDate) - new Date(secondDate)) / oneDay));
+return Math.floor(((new Date(firstDate) - new Date(secondDate)) / oneDay));
 
 }
