f4a837a00be8c762bd0778b30d40150db469776e
Tim Blanchard <timothy.blanchard@tesco.com>
2021-04-18 11:24:53 +0100

    extracted helper fns to schedulemapper

--- src/helpers/scheduleMapper.js
+++ src/helpers/scheduleMapper.js
@@ -1,9 +1,21 @@
+import localTimeMapper from "./localTimeMapper";
+import deltaTimeMapper from "./deltaTimeMapper";
 export default function scheduleMapper({
   MRData: {
     RaceTable: { Races: races },
   },
 }) {
-  return races && races.map(({raceName, date, time}, key) => {
-   return {key,raceName, date, time, }
-  });
+  return (
+    races &&
+    races.map(({ raceName, date, time }, key) => {
+      return {
+        key,
+        raceName,
+        date,
+        time,
+        localTime: localTimeMapper({ date, time }),
+        deltaTime: deltaTimeMapper({ date }),
+      };
+    })
+  );
 }
--- src/helpers/scheduleMapper.test.js
+++ src/helpers/scheduleMapper.test.js
@@ -1,40 +1,53 @@
 import { mockSchedule } from "../mocks/mockSchedule";
 import scheduleMapper from "./scheduleMapper";
+jest.mock("./localTimeMapper", () => () => "a local time");
+jest.mock("./deltaTimeMapper", () => () => "mock days to go");
 
 const expected = [
   {
     date: "2021-03-28",
+    deltaTime: "mock days to go",
     key: 0,
+    localTime: "a local time",
     raceName: "Bahrain Grand Prix",
     time: "15:00:00Z",
   },
   {
     date: "2021-04-18",
+    deltaTime: "mock days to go",
     key: 1,
+    localTime: "a local time",
     raceName: "Emilia Romagna Grand Prix",
     time: "13:00:00Z",
   },
   {
     date: "2021-05-02",
+    deltaTime: "mock days to go",
     key: 2,
+    localTime: "a local time",
     raceName: "Portuguese Grand Prix",
     time: "13:00:00Z",
   },
   {
     date: "2021-05-09",
+    deltaTime: "mock days to go",
     key: 3,
+    localTime: "a local time",
     raceName: "Spanish Grand Prix",
     time: "13:00:00Z",
   },
   {
     date: "2021-05-23",
+    deltaTime: "mock days to go",
     key: 4,
+    localTime: "a local time",
     raceName: "Monaco Grand Prix",
     time: "13:00:00Z",
   },
 ];
 describe("scheduleMapper", () => {
   it("should retun expected data", () => {
+    console.log(expected);
     expect(scheduleMapper(mockSchedule)).toEqual(expected);
   });
 });
